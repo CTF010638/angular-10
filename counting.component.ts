f9e7643ff91a4d8036b60135cd4e48e433dca97d
CTF010638 <80992442+CTF010638@users.noreply.github.com>
2021-03-20 08:16:26 +0700

    Add files via upload

---
+++ counting.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-counting',
+    templateUrl: './counting.component.html',
+    styleUrls: ['./counting.component.css']
+  })
+
+
+export class CountingComponent {
+    count: number = 0;
+    countNumber(){
+        this.count = this.count +1;
+
+    }
+    
+  }
+  
